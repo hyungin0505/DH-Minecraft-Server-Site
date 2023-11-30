750a239124fb1832442f21d8d641b10ba8dc595d
KoGandhi <78721669+hyungin0505@users.noreply.github.com>
2023-12-01 01:24:29 +0900

    페이지 업데이트
    안내, 링크 및 규칙 업데이트

---
+++ javascript/copy.js
@@ -0,0 +1,10 @@
+function copy(id)
+{
+var r = document.createRange();
+r.selectNode(document.getElementById(id));
+window.getSelection().removeAllRanges();
+window.getSelection().addRange(r);
+document.execCommand('copy');
+window.getSelection().removeAllRanges();
+alert("복사되었습니다.")
+}
